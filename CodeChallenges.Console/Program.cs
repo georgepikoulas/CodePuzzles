ba55bc7b3835e149506ff26e57927188bc1b7563
George Pikoulas <pikoulas@fortunethellas.gr>
2020-10-13 22:35:18 +0300

    Added visual console   messagees

--- CodeChallenges.Console/Program.cs
+++ CodeChallenges.Console/Program.cs
@@ -1,22 +1,51 @@
 ﻿using CodeChallenges.ConsoleUI.Challenges;
 using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
 namespace CodeChallenges.ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.Write("Enter the Number= ");
+            // Initialize a new instance of the SpeechSynthesizer.  
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+
+
+            Console.Write("Βάλε Αριθμό= ");
             var input = Console.ReadLine();
             var armstrong = new ArmstrongNumber();
 
             var isArmstrong = armstrong.Calculate(input);
 
-            if(isArmstrong)
-                Console.Write("Armstrong Number.");
+            if (isArmstrong)
+            {
+                Console.BackgroundColor =  ConsoleColor.DarkGreen ;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Είναι Armstrong Number.");
+                Console.ResetColor();
+            }
             else
-                Console.Write("Not Armstrong Number.");
+            {
+                Console.BackgroundColor =  ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Δεν είναι Armstrong Number.");
+                Console.ResetColor();
+
+            }
+
+            Console.ReadKey();
+        }
+
+        static async Task SynthesizeAudioAsync()
+        {
+            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+            using var synthesizer = new SpeechSynthesizer(config);
+            await synthesizer.SpeakTextAsync("Synthesizing directly to speaker output.");
         }
     }
 }
